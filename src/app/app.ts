9a7573a4833c87fda4cd13f174bbe3cd476a6041
hariniramesh235711# <2005hariniramesh@gmail.com>
2026-02-26 17:57:23 +0530

    added readme file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `
+    <h1>Exam Proctoring System</h1>
+    <router-outlet></router-outlet>
+  `,
 })
 export class App {
-  protected readonly title = signal('exam-proctoring-frontend');
 }
