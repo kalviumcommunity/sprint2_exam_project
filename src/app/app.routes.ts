1add609631be1b9c8b5dfa5540eda5c16b1dfd82
hariniramesh235711# <2005hariniramesh@gmail.com>
2026-03-03 12:51:42 +0530

    feat: add register, dashboard, exam, result, navbar components and setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Dashboard } from './dashboard/dashboard';
+import { Exam } from './exam/exam';
+import { Result } from './result/result';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Login },
+  { path: 'register', component: Register },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'exam', component: Exam },
+  { path: 'result', component: Result },
+];
